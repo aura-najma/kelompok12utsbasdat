1a3d4fade5de979bdb3f5a064753b6e674b47d9d
Dyah <dyahayuretnoningsih22@gmail.com>
2024-10-05 14:32:58 +0700

    19

--- routes/web.php
+++ routes/web.php
@@ -101,8 +101,8 @@ Route::post('/evaluasi', [EvaluasiController::class, 'store'])->name('evaluasi.s
 
 use App\Http\Controllers\DokterController;
 
-Route::get('/validasi-dokter', [DokterController::class, 'index'])->name('validasi.dokter');
-Route::get('/dokter', [DokterController::class, 'index']);
+Route::get('/validasidokter', [DokterController::class, 'index']);
+
 
 // bagian aura
 use App\Http\Controllers\PasienController;

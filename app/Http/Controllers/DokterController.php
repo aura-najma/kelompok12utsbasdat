d048467b80432983ef1757541d504cc12e23032d
Dyah <dyahayuretnoningsih22@gmail.com>
2024-10-05 13:22:34 +0700

    18

--- app/Http/Controllers/DokterController.php
+++ app/Http/Controllers/DokterController.php
@@ -7,12 +7,12 @@ use DB; // Pastikan Anda mengimpor DB facade
 
 class DokterController extends Controller
 {
-    public function index()
-    {
-        // Mengambil data dari tabel dokter
-        $dokters = DB::table('dokter')->get();
 
-        // Mengirim data dokter ke view validasidokter.blade.php
-        return view('validasidokter', ['dokters' => $dokters]);
-    }
+    public function index()
+{
+    $dokters = DB::table('dokter')->get();
+    dd($dokters); // Ini akan memeriksa apakah $dokters sudah ada isinya
+    return view('validasidokter', ['dokters' => $dokters]);
+}
+
 }

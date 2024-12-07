3eafc79d4b132c6ea9e8aa8f6e5d7d54ce55ec69
Dyahayuretnoningsih <dyahayuretnoningsih.official@gmail.com>
2024-12-07 21:17:30 +0700

    apoteker

--- database/seeders/ApotekerSeeder.php
+++ database/seeders/ApotekerSeeder.php
@@ -15,7 +15,7 @@ class ApotekerSeeder extends Seeder
     {
         DB::table('apoteker')->insert([
             [
-                'NIP' => 1,
+                'NIP' => 12024,
                 'password' => Hash::make('katak'), // Hashing the password for security
                 'user' => 'Dyah Ayu',
                 'no_hp' => '082335467428',
@@ -26,13 +26,35 @@ class ApotekerSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'NIP' => 2,
+                'NIP' => 22024,
                 'password' => Hash::make('kupu'), // Hashing the password for security
                 'user' => 'Aura Najma',
                 'no_hp' => '082335460029',
                 'email' => 'auranajma@gmail.com',
                 'alamat' => 'Jl Sutorejo 16 Mulyorejo Surabaya',
                 'jam_kerja' => '12.00 - 17.00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'NIP' => 32024,
+                'password' => Hash::make('kuda'), // Hashing the password for security
+                'user' => 'Wanda Desi',
+                'no_hp' => '082335460028',
+                'email' => 'wandadesi@gmail.com',
+                'alamat' => 'Jl Sutorejo 15 Mulyorejo Surabaya',
+                'jam_kerja' => '17.00 - 23.00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'NIP' => 42024,
+                'password' => Hash::make('keledai'), // Hashing the password for security
+                'user' => 'Zulfikar',
+                'no_hp' => '082335460020',
+                'email' => 'zulfikar@gmail.com',
+                'alamat' => 'Jl Sutorejo 14 Mulyorejo Surabaya',
+                'jam_kerja' => '23.00 - 07.00',
                 'created_at' => now(),
                 'updated_at' => now(),
             ]

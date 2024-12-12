186d678785df7d2fee1e11718f85d40a7cddc5d1
aura-najma <aurakuliahftmmmunair@gmail.com>
2024-12-12 19:48:02 +0700

    perbaiki evaluasi factory

--- database/factories/EvaluasiFactory.php
+++ database/factories/EvaluasiFactory.php
@@ -3,10 +3,11 @@
 namespace Database\Factories;
 
 use App\Models\Pasien;
-use App\Models\Transaksi; 
-use App\Models\Evaluasi; // Pastikan model Transaksi diimport
+use App\Models\Transaksi;
+use App\Models\Evaluasi;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Carbon\Carbon; // Import Carbon untuk manipulasi tanggal
+use Carbon\Carbon;
+use Illuminate\Support\Str; // Import Str untuk string acak
 
 class EvaluasiFactory extends Factory
 {
@@ -21,13 +22,19 @@ class EvaluasiFactory extends Factory
         $evaluasiOptions = ['Sangat Kurang', 'Kurang', 'Cukup', 'Baik', 'Sangat Baik'];
         $komentarOptions = ['baik', '-', 'sip', 'mantap', 'perlu diperbaiki', 'overall oke tingkatkan terus', 'sangat jelek', 'apotek aneh'];
 
-        // Ambil ID invoice dan tanggal transaksi acak dari tabel transaksis
-        $transaksi = Transaksi::inRandomOrder()->first(); // Mengambil transaksi acak
-        $id_invoice = $transaksi->id_invoice ; // Ambil ID invoice dari tabel transaksi
+        // Ambil ID invoice acak dari tabel evaluasi yang belum ada
+        do {
+            $transaksi = Transaksi::inRandomOrder()->first(); // Mengambil transaksi acak
+            $id_invoice = $transaksi->id_invoice ; // Ambil ID invoice dari tabel transaksi
+        } while (Evaluasi::where('id_invoice', $id_invoice)->exists()); // Pastikan id_invoice belum ada di tabel evaluasi
+
         $tanggal_transaksi = $transaksi->created_at ; // Ambil tanggal transaksi dari tabel transaksis atau gunakan waktu sekarang jika tidak ada transaksi
 
         // Mengatur created_at untuk diacak antara tanggal transaksi, tanggal transaksi +1, atau tanggal transaksi +2
         $createdAt = Carbon::parse($tanggal_transaksi)->addDays(rand(0, 2)); // Tambahkan 0, 1, atau 2 hari
+
+        // Format id_evaluasi seperti EV-{3 karakter acak}-{tanggal transaksi}
+        $id_evaluasi = 'EV-' . Str::random(3) . '-' . Carbon::parse($tanggal_transaksi)->format('Ymd');
 
         // Return data dummy
         return [
@@ -40,7 +47,7 @@ class EvaluasiFactory extends Factory
             'komentar' => $faker->randomElement($komentarOptions),
             'created_at' => $createdAt, // Menggunakan created_at yang diacak
             'updated_at' => $createdAt, // Set updated_at sama dengan created_at
-            'id_evaluasi' => $faker->uuid(),
+            'id_evaluasi' => $id_evaluasi, // id_evaluasi dengan format yang diinginkan
             'evaluasi_dibuat' => null,
             'evaluasi_diperbarui' => null,
         ];
